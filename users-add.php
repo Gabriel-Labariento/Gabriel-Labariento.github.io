1b1b5fff8a4a5c80ef91b0f340bb1e644661296b
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2024-03-02 07:59:45 +0800

    Update user function added.

--- users-add.php
+++ users-add.php
@@ -104,7 +104,7 @@
                                         <td><?= date('M d, Y @ h:i:s A', strtotime($user['updated_at'])) ?></td>
                                         <td><?= date('M d, Y @ h:i:s A', strtotime($user['created_at'])) ?></td>
                                         <td>
-                                            <a href="" class="updateUser"><i class="fa fa-pencil"></i> Edit<br></a>                                                
+                                            <a href="" class="updateUser" data-userid="<?= $user['id'] ?>"><i class="fa fa-pencil"></i> Edit<br></a>                                                
                                             <a href="" class="deleteUser" data-userid="<?= $user['id'] ?>" data-fname="<?= $user['first_name'] ?>" data-lname="<?= $user['last_name'] ?>"> <i class="fa fa-trash"></i> Delete</a>                                    
                                         </td>
                                     </tr>
@@ -123,6 +123,59 @@
         </div>
     </div>
    </div>
+   
+   <div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to perform this action?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" id="confirmAction">Confirm</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="successModalLabel">Successfully Updated</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                User was successfully updated. Please refresh the page to see changes.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Okay</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="errorModal" tabindex="-1" aria-labelledby="errorModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="errorModalLabel">Error</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                There was an error in processing user update.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Okay</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
    <script src="js/script.js"></script>
    <script src="js/jquery/jquery-3.7.1.min.js"></script>
@@ -130,59 +183,57 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.35.4/css/bootstrap-dialog.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.35.4/js/bootstrap-dialog.min.js"></script>
 
-   <script>
-        function script(){
+    <script>
+    function script() {
+        this.initialize = function () {
+            this.registerEvents();
+        },
 
-            this.initialize = function(){
-                this.registerEvents();
-            },
+        this.registerEvents = function () {
+            document.addEventListener('click', function (e) {
+                targetElement = e.target;
+                classList = targetElement.classList;
 
-            this.registerEvents = function(){
-                document.addEventListener('click', function(e){
-                    targetElement = e.target;
-                    classList = targetElement.classList;
+                if (classList.contains('deleteUser')) {
+                    e.preventDefault();
+                    userId = targetElement.dataset.userid;
+                    fname = targetElement.dataset.fname;
+                    lname = targetElement.dataset.lname;
+                    fullName = fname + ' ' + lname;
 
-                    if(classList.contains('deleteUser')){
-                        e.preventDefault();
-                        userId = targetElement.dataset.userid;
-                        fname = targetElement.dataset.fname;
-                        lname = targetElement.dataset.lname;
-                        fullName = fname + ' ' + lname;
-
-                        if (window.confirm('Are you sure to delete '+ fullName + '?')){
-                            $.ajax({
-                                method: 'POST',
-                                data:  {
-                                    user_id: userId,
-                                    f_name: fname,
-                                    l_name: lname
-                                },
-                                url: 'database/delete-user.php',
-                                dataType:'json',
-                                success: function(data){
-                                    if(data.success){
-                                        if(window.alert(data.message)){
-                                            location.reload();
-                                        }
-                                        else window.alert(data.message);
-                                    }
+                    if (window.confirm('Are you sure to delete ' + fullName + '?')) {
+                        $.ajax({
+                            method: 'POST',
+                            data: {
+                                user_id: userId,
+                                f_name: fname,
+                                l_name: lname
+                            },
+                            url: 'database/delete-user.php',
+                            dataType: 'json',
+                            success: function (data) {
+                                if (data.success) {
+                                    if (window.alert(data.message)) {
+                                        location.reload();
+                                    } else window.alert(data.message);
                                 }
-                            })
-                        } else{
-                            console.log('will not delete.');
-                        } 
+                            }
+                        })
+                    } else {
+                        console.log('will not delete.');
                     }
+                }
 
-                    if(classList.contains('updateUser')){
-                        e.preventDefault();
+                if (classList.contains('updateUser')) {
+                    e.preventDefault();
 
-                        firstName = targetElement.closest('tr').querySelector('td.firstName').innerHTML;
-                        lastName = targetElement.closest('tr').querySelector('td.lastName').innerHTML;
-                        email  = targetElement.closest('tr').querySelector('td.email').innerHTML;
+                    firstName = targetElement.closest('tr').querySelector('td.firstName').innerHTML;
+                    lastName = targetElement.closest('tr').querySelector('td.lastName').innerHTML;
+                    email = targetElement.closest('tr').querySelector('td.email').innerHTML;
+                    userId = targetElement.dataset.userid;
 
-                        BootstrapDialog.confirm({
-                            title: 'Update' + firstName + ' ' + lastName,
-                            message: '<form>\
+                    var modalBody = document.querySelector('#confirmationModal .modal-body');
+                        modalBody.innerHTML = '<form>\
                             <div class="form-group">\
                                 <label for="firstName">First Name:</label>\
                                 <input type="text" class="form-control" id="firstName" placeholder="Enter first name" value="'+ firstName + '">\
@@ -192,19 +243,58 @@
                                 <input type="text" class="form-control" id="lastName" placeholder="Enter last name" value="'+ lastName + '">\
                             </div>\
                             <div class="form-group">\
-                                <label for="firstName">First Name:</label>\
-                                <input type="email" class="form-control" id="email" placeholder="Enter new email" value="'+ email + '">\
+                                <label for="email">Email:</label>\
+                                <input type="email" class="form-control" id="emailUpdate" placeholder="Enter new email" value="'+ email + '">\
                             </div>\
-                            </form>'
+                        </form>';
+
+                        // Show the modal
+                        var modal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+                        modal.show();
+
+                    // Add event listener for the confirmation button inside the modal
+                    document.getElementById('confirmAction').addEventListener('click', function () {
+                        modal.hide();
+                   // Perform AJAX request
+                            $.ajax({
+                                method: 'POST',
+                                data: {
+                                    user_id: userId,
+                                    f_name: document.getElementById('firstName').value,
+                                    l_name: document.getElementById('lastName').value,
+                                    email: document.getElementById('emailUpdate').value
+                                },
+                                url: 'database/update-user.php', // Adjust URL as needed
+                                dataType: 'json',
+                                success: function (data) {
+                                    if (data.success) {
+                                        // Success message
+                                        var successModal = new bootstrap.Modal(document.getElementById('successModal'));
+                                        successModal.show();
+                                        successModal.addEventListener('hidden.bs.modal', function () {
+                                        location.reload();
+                                        });
+                                        
+                                    } else {
+                                        // Error message
+                                        var errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+                                        errorModal.show();  
+                                    }
+                                },
+                                error: function (xhr, status, error) {
+                                    // Handle error if AJAX request fails
+                                    console.error(xhr.responseText);
+                                }
+                            });
                         });
-                    }
-                })
-
-            }
+                }
+            });
         }
+    }
 
-        var script = new script;
-        script.initialize();
-   </script>
+    var script = new script;
+    script.initialize();
+</script>
 </body>
 </html>
+
